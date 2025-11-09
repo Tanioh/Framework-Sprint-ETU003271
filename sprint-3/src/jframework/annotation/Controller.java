59c7e4408065dacd361a894aa7ca5eadc42bd784
Tanioh <taniohsteven@gmail.com>
2025-11-09 20:08:42 +0300

    sprint 3 update

--- sprint-3/src/jframework/annotation/Controller.java
+++
@@ -1,8 +0,0 @@
-package jframework.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME) // doit être accessible à l'exécution
-public @interface Controller {
-}
