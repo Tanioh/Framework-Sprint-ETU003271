49bfc78c7731dbac3606bc55b95255b9ca3be2d3
tanioh <tanioh@users.noreply.github.com>
2026-01-13 14:12:12 +0100

    Sprint 8 bis: ajout build/lib/src et scripts

---
+++ sprint-8-bis/src/framework/annotation/Controller.java
@@ -0,0 +1,9 @@
+package framework.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // doit Ãªtre accessible Ã  l'exÃ©cution
+public @interface Controller {
+}
+
