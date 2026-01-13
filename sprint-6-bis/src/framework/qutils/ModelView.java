aac36c00fb5f66ccef4e7f2e63e133bd0cb34423
Tanioh <tanioh@users.noreply.github.com>
2026-01-13 12:43:06 +0100

    Sprint 6 bis: migration du framework (renommage jframework -> framework)

---
+++ sprint-6-bis/src/framework/qutils/ModelView.java
@@ -0,0 +1,23 @@
+package framework.qutils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+    String view;
+    HashMap<String, Object> data = new HashMap<>(); 
+    public void setView(String view) {
+        this.view = view;
+    }
+    public String getView() {
+        return view;
+    }
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void addData(String key, Object data) {
+        this.data.put(key, data);
+    }
+}
