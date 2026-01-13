fc4cced0075cc9b4e9f409b97dcac73b1565a039
Tanioh <tanioh@users.noreply.github.com>
2026-01-13 13:22:47 +0100

    Sprint 8

---
+++ sprint-8/src/framework/annotation/Controller.java
@@ -0,0 +1,8 @@
+package framework.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // doit être accessible à l'exécution
+public @interface Controller {
+}
